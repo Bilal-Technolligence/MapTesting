49487fea6ec67048959365fae58e682e9d7a3708
Aashir-Technolligence <aashi7651@gmail.com>
2020-04-23 14:42:53 +0500

    initial

--- app/src/main/java/com/example/maptesting/SplashScreenActivity.java
+++ app/src/main/java/com/example/maptesting/SplashScreenActivity.java
@@ -1,8 +1,11 @@
 package com.example.maptesting;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -17,7 +20,13 @@ public class SplashScreenActivity extends AppCompatActivity {
         ImageView imageView = findViewById( R.id.imagelogo );
         Animation animation = AnimationUtils.loadAnimation( getApplicationContext(),R.anim.fade );
         imageView.startAnimation( animation );
-
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            recreate();
+            return;
+        }
         Thread timer = new Thread(  ) {
             @Override
             public void run() {
